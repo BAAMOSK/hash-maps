817ad7152fa6b14e8263ab715da0f4ad62ad76bb
Zach Williams <saumanahaii@gmail.com>
2017-06-26 14:54:02 -0400

    implemented linked lists in hashtable

--- hash.js
+++ hash.js
@@ -1,100 +1,100 @@
 class HashMap {
-    //Initial data structure is an array
-    constructor(initialCapacity=8) {
-        //no length
-        this.length = 0;
-        //empty array
-        this._slots = [];
-        //capacity is arg or 8
-        this._capacity = initialCapacity;
-        this.deleted = 0;
+  //Initial data structure is an array
+  constructor(initialCapacity = 8) {
+      //no length
+      this.length = 0;
+      //empty array
+      this._slots = [];
+      //capacity is arg or 8
+      this._capacity = initialCapacity;
+      this.deleted = 0;
     }
     //this hashes the KEY
-    static _hashString(string) {
-        //prime number
-        let hash = 5381;
+  static _hashString(string) {
+      //prime number
+      let hash = 5381;
 
-        for (let i=0; i<string.length; i++) {
-            //converting the asci code
-            // console.log(string.charCodeAt(i));
-            hash = (hash << 5) + hash + string.charCodeAt(i);
-            // console.log(hash);
-            //Makes sure that hash is a positive number
-            //The & is a bitwise operator
-            hash = hash & hash;
-        }
-        //returns a positive number
-        return hash >>> 0;
+      for (let i = 0; i < string.length; i++) {
+        //converting the asci code
+        // console.log(string.charCodeAt(i));
+        hash = (hash << 5) + hash + string.charCodeAt(i);
+        // console.log(hash);
+        //Makes sure that hash is a positive number
+        //The & is a bitwise operator
+        hash = hash & hash;
+      }
+      //returns a positive number
+      return hash >>> 0;
     }
     //adding KEY VALUE to hashmap
-    set(key, value) {
-        //computes load ratio
-        const loadRatio = (this.length + 1) / this._capacity;
-        //if close to 90% will increase size
-        if (loadRatio > HashMap.MAX_LOAD_RATIO) {
-            this._resize(this._capacity * HashMap.SIZE_RATIO);
-        }
-        //index is KEY
-        const index = this._findSlot(key);
-        //finds slot
-        this._slots[index] = {
-            key,
-            value
-        };
-        this.length++;
+  set(key, value) {
+    //computes load ratio
+    const loadRatio = (this.length + 1) / this._capacity;
+    //if close to 90% will increase size
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
     }
-    get(key) {
-        const index = this._findSlot(key);
-        if (this._slots[index] === undefined) {
-          //throw new Error('Key error');
-          return undefined;
-        }
-        return this._slots[index].value;
+    //index is KEY
+    const index = this._findSlot(key);
+    //finds slot
+    this._slots[index] = {
+      key,
+      value
+    };
+    this.length++;
+  }
+  get(key) {
+    const index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+      //throw new Error('Key error');
+      return undefined;
+    }
+    return this._slots[index].value;
+  }
+  _findSlot(key) {
+    //takes key and hashes it
+    const hash = HashMap._hashString(key);
+    //starting index of key
+    const start = hash % this._capacity;
+    // console.log(start);
+    for (let i = start; i < start + this._capacity; i++) {
+      const index = i % this._capacity;
+      const slot = this._slots[index];
+      //finds if key is empty
+      if (slot === undefined || slot.key == key) {
+        return index;
       }
-    _findSlot(key) {
-        //takes key and hashes it
-        const hash = HashMap._hashString(key);
-        //starting index of key
-        const start = hash % this._capacity;
-        // console.log(start);
-        for (let i=start; i<start + this._capacity; i++) {
-            const index = i % this._capacity;
-            const slot = this._slots[index];
-            //finds if key is empty
-            if (slot === undefined || slot.key == key) {
-                return index;
-            }
-        }
     }
-    remove(key) {
-            const index = this._findSlot(key);
-            //index
-            const slot = this._slots[index];
-            //if empty won't delete anything
-            if (slot === undefined) {
-                throw new Error('Key error');
-            }
-
-            slot.deleted = true;
-            this.length--;
-            this._deleted++;
-        }
-
-    _resize(size) {
-        //makes a reference to old array
-        const oldSlots = this._slots;
-        this._capacity = size;
-        // Reset the length - it will get rebuilt as you add the items back
-        this.length = 0;
-        this._slots = [];
-        //reiterate
-        for (const slot of oldSlots) {
-            if (slot !== undefined) {
-                //rehashes based on size of 24 instead of 8
-                this.set(slot.key, slot.value);
-            }
-        }
+  }
+  remove(key) {
+    const index = this._findSlot(key);
+    //index
+    const slot = this._slots[index];
+    //if empty won't delete anything
+    if (slot === undefined) {
+      throw new Error('Key error');
     }
+
+    slot.deleted = true;
+    this.length--;
+    this._deleted++;
+  }
+
+  _resize(size) {
+    //makes a reference to old array
+    const oldSlots = this._slots;
+    this._capacity = size;
+    // Reset the length - it will get rebuilt as you add the items back
+    this.length = 0;
+    this._slots = [];
+    //reiterate
+    for (const slot of oldSlots) {
+      if (slot !== undefined) {
+        //rehashes based on size of 24 instead of 8
+        this.set(slot.key, slot.value);
+      }
+    }
+  }
 }
 //90% of array filled
 HashMap.MAX_LOAD_RATIO = 0.9;
@@ -154,9 +154,10 @@ let words = ['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race'];
 
 function anagrams(key) {
   const hm = new HashMap();
-  for(let i = 0; i < key.length; i++) {
-    let word = key[i].split('').sort((a,b) => a.charCodeAt(0) - b.charCodeAt(0)).join('');
-    if(hm.get(word) === undefined) {
+  for (let i = 0; i < key.length; i++) {
+    let word = key[i].split('').sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0))
+      .join('');
+    if (hm.get(word) === undefined) {
       hm.set(word, [key[i]]);
     } else {
       let value = hm.get(word);
@@ -169,4 +170,4 @@ function anagrams(key) {
   return hm;
 };
 
-console.log(anagrams(words));
+//console.log(anagrams(words));
---
+++ hash2.js
@@ -0,0 +1,98 @@
+class Hash {
+  constructor() {
+    this.length = 0;
+    this._slots = [];
+    this._capacity = 8;
+    this._deleted = 0;
+  }
+  static _hashString(string) {
+
+    //prime number
+    let hash = 5381;
+    for (let i = 0; i < string.length; i++) {
+      //converting the asci code
+      // console.log(string.charCodeAt(i));
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      // console.log(hash);
+      //Makes sure that hash is a positive number
+      //The & is a bitwise operator
+      hash = hash & hash;
+    }
+    //returns a positive number
+    return hash >>> 0;
+  }
+
+  set(key, value, parent) {
+    //computes load ratio
+    const loadRatio = (this.length + 1) / this._capacity;
+    //if close to 90% will increase size
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
+    }
+    //index is KEY
+    let theSlot = this._findSlot(key);
+    const index = theSlot[0];
+    const prev = theSlot[1];
+    //finds slot
+    let prevObj = this._slots[prev];
+
+    if (typeof prevObj !== "undefined") {
+      prevObj.next = index;
+    }
+
+    this._slots[prev] = prevObj;
+
+    this._slots[index] = {
+      key,
+      value,
+      next: null
+    };
+
+
+
+    this.length++;
+  }
+
+  get(key) {
+    const index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+      //throw new Error('Key error');
+      return undefined;
+    }
+    return this._slots[index].value;
+  }
+  _findSlot(key) {
+    let prevEl = null;
+    //takes key and hashes it
+    const hash = HashMap._hashString(key);
+    //starting index of key
+    const start = hash % this._capacity;
+    // console.log(start);
+    for (let i = start; i < start + this._capacity; i++) {
+      let index = i % this._capacity;
+      let slot = this._slots[index];
+      let originalSlot = slot;
+      //finds if key is empty
+
+      //console.log((slot === undefined || slot.key == key))
+
+      while (!(slot === undefined)) {
+        prevEl = index;
+        index++;
+        slot = this._slots[index];
+
+      }
+
+      return [index, prevEl];
+    }
+  }
+}
+
+let x = new Hash();
+x.set("foo", "buzz");
+x.set("foo", "bar");
+x.set("Bizz", "beetle");
+x.set("Bizz", "bop");
+x.set("ars", "technica");
+x.set("prosthetic", "knowledge");
+console.log(x);
